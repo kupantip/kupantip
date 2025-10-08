936c03a3010861a4ef8b74a74dbd383b00b0cc73
Paranyu Kittinavakit <132139926+ParanyuLion@users.noreply.github.com>
2025-10-08 20:51:41 +0700

    Update backend/src/routes/report.route.ts
    Co-authored-by: Earth <109275765+Xeei@users.noreply.github.com>

--- backend/src/routes/report.route.ts
+++ backend/src/routes/report.route.ts
@@ -8,7 +8,7 @@ import {
 
 const router = Router();
 
-router.get('/', listReportsController);
+router.get('/', authMiddleware, listReportsController);
 
 router.post('/', authMiddleware, createReportController);
 
