c227b8964d9c486d69e87ade6ade4c7af7abd2d4
MunyinSam <kelvinsam233@gmail.com>
2025-11-17 11:34:03 +0700

    feat(profile): implement user profile layout and my posts page with loading and error handling

--- frontend/src/components/ProfileDropdown.tsx
+++ frontend/src/components/ProfileDropdown.tsx
@@ -81,7 +81,7 @@ export default function ProfileDropDown() {
 							</Link>
 						</DropdownMenuItem>
 						<DropdownMenuItem asChild disabled={!isAuthenticated}>
-							<Link href="/my-posts" className="cursor-pointer">
+							<Link href="/profile/my-posts" className="cursor-pointer">
 								<FileText className="mr-2 h-4 w-4" />
 								<span>My Posts</span>
 							</Link>
